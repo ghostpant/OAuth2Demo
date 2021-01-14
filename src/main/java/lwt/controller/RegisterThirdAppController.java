cc53cf7fd5d76a11652860eca25c14a8400b0c78
liwei_tao <250056@kingdee.comm>
2021-01-14 20:36:29 +0800

    logBack

--- src/main/java/lwt/controller/RegisterThirdAppController.java
+++ src/main/java/lwt/controller/RegisterThirdAppController.java
@@ -1,25 +1,25 @@
 package lwt.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import lwt.builder.ApiResultBuilder;
 import lwt.info.query.RedirectUrlQuery;
 import lwt.info.query.ThirdAppRegisterQuery;
 import lwt.serviceImpl.CodeRedirectServiceImpl;
 import lwt.serviceImpl.RegistThirdAppServiceImpl;
 import lwt.utls.ApiResult;
+import org.apache.log4j.MDC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
 
-@RestController
+@Controller
 public class RegisterThirdAppController {
 
     private static final Logger log = LoggerFactory.getLogger(RegisterThirdAppController.class);
@@ -34,17 +34,20 @@ public class RegisterThirdAppController {
     }
 
     @RequestMapping(value = "third/app/register/getApp", method = RequestMethod.POST)
+    @ResponseBody
     public ApiResult registThirdApp(@Valid @RequestBody ThirdAppRegisterQuery query) {
         String tag = "获取AppId和AppSecert";
-        log.info("【{}】 请求参数", tag, query);
+        String trace = (String) MDC.get("trace");
+        log.info("【{}】,trace = {}, 请求参数 = {}", tag,trace, query);
         ApiResult appIdSecert = registThirdAppService.getAppIdSecert(query);
-        log.info("【{}】请求结果: result = {}", tag, appIdSecert);
+        log.info("【{}】请求结果: result = {}", tag, JSONObject.toJSONString(appIdSecert));
         return appIdSecert;
     }
 
     @RequestMapping(value = "get/auth/page",method = RequestMethod.GET)
     public ApiResult redirectUrl2Auth(HttpServletRequest request, HttpServletResponse response) {
         String tag = "重定向到授权页面";
+        String trace = (String) MDC.get("trace");
         String appid = request.getParameter("appid");
         String appNumber = request.getParameter("appNumber");
         String redirect_url = request.getParameter("redirect_url");
@@ -53,11 +56,9 @@ public class RegisterThirdAppController {
         RedirectUrlQuery query = RedirectUrlQuery.builder().appid(appid).appNumber(appNumber).redirect_url(redirect_url).auth_type(auth_type).build();
 
         boolean checkCodeRedirectParams = codeRedirectService.checkCodeRedirectParams(query);
-
-
         if (checkCodeRedirectParams) {
             try {
-                response.sendRedirect(redirect_url + "?code="+123123);
+                response.sendRedirect(redirect_url);
             } catch (IOException e) {
                 e.printStackTrace();
             }
